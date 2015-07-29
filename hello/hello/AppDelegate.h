066f24c84d7b31fc39307ab550523407a46d31db
lengpeng <123@163.com>
2015-07-29 20:14:57 +0800

    first

---
+++ hello/hello/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  hello
+//
+//  Created by lengpeng on 15/7/29.
+//  Copyright (c) 2015年 lengpeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ hello/hello/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  hello
+//
+//  Created by lengpeng on 15/7/29.
+//  Copyright (c) 2015年 lengpeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
